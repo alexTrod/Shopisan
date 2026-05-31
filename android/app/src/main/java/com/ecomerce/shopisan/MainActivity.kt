f20d3751fe708d9966536e39df45bfc6ef711dec
Gansovic <knezevicoluka@gmail.com>
2026-05-31 14:24:37 +0200

    chore(android): rename package to com.ecomerce.shopisan and update splash logos

--- android/app/src/main/java/com/ecomerce/shopisan/MainActivity.kt
+++ android/app/src/main/java/com/ecomerce/shopisan/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.lukini.Shopisanv1
+package com.ecomerce.shopisan
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/ecomerce/shopisan/MainApplication.kt
+++ android/app/src/main/java/com/ecomerce/shopisan/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.lukini.Shopisanv1
+package com.ecomerce.shopisan
 
 import android.app.Application
 import android.content.res.Configuration
